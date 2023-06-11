3dc3160203add9b97c8a776bea6943061b9800c9
Nicolo <nicolo.grilli@bristol.ac.uk>
2023-06-11 17:29:03 +0100

    plane stress triangle

--- planestresstriangle.py
+++ planestresstriangle.py
@@ -1,13 +1,13 @@
 # Nicolò Grilli
 # University of Bristol
-# 10 Giugno 2023
+# 11 Giugno 2023
 
-# a plane strain triangle element
-# Notation CPE3 used in Abaqus
+# a plane stress triangle element
+# Notation CPS3 used in Abaqus
 
 import numpy as np
 
-class PlainStrainTriangle(ConstantStrainTriangle):
+class PlainStressTriangle(ConstantStrainTriangle):
 	
 	def __init__(self,nodes,material):
 		super().__init__(nodes,material)
@@ -15,12 +15,12 @@ class PlainStrainTriangle(ConstantStrainTriangle):
 	# calculate material stiffness matrix
 	def calculate_D(self):
 		self.D = np.zeros(shape=(3,3))
-		self.D[0,0] = 1.0 - self.poisson_ratio
+		self.D[0,0] = 1.0
 		self.D[0,1] = self.poisson_ratio
 		self.D[1,0] = self.poisson_ratio
-		self.D[1,1] = 1.0 - self.poisson_ratio
-		self.D[2,2] = 0.5 - self.poisson_ratio
-		self.D *= self.young_modulus / (1.0 + self.poisson_ratio) / (1.0 - 2.0 * self.poisson_ratio)
+		self.D[1,1] = 1.0
+		self.D[2,2] = (1.0 - self.poisson_ratio) / 2.0
+		self.D *= self.young_modulus / (1.0 - self.poisson_ratio * self.poisson_ratio)
 
 	# calculate full element stiffness matrix
 	def calculate_stiffness_matrix(self):
